75bdbf3aa0b081abdb218de814125be540caaacb
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2013-12-10 21:17:17 +0900

    define LOG_WARN as LOG_WARNING in order to make it shorter.

--- lib/kcn_log.h
+++ lib/kcn_log.h
@@ -7,7 +7,9 @@
 #define KCN_LOG(...)		_KCN_LOG(__VA_ARGS__, "")
 #else /* HAVE_C99_VA_ARGS */
 #error cpp does not support __VA_ARGS__ or kcn.h is not properly included.
-#endif /* ! HAVE_GCC_VA_ARGS && ! HAVE_C99_VA_ARGS */ 
+#endif /* ! HAVE_GCC_VA_ARGS && ! HAVE_C99_VA_ARGS */
+
+#define LOG_WARN	LOG_WARNING
 
 void kcn_log_priority_increment(void);
 void kcn_log(int, const char *, ...);
