a0984ceba56e34036975d951cb780ba6ed75c01f
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2013-12-17 10:43:35 +0900

    add definitions of constant values.

--- lib/kcn_netstat.h
+++ lib/kcn_netstat.h
@@ -1,4 +1,6 @@
+#define KCN_NETSTAT_SERVER_STR_DEFAULT	"127.0.0.1"	/* XXX */
 #define KCN_NETSTAT_PORT_DEFAULT	9410
+#define KCN_NETSTAT_PORT_STR_DEFAULT	"9410"
 
 void kcn_netstat_init(void);
 void kcn_netstat_finish(void);
