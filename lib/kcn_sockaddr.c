6a13214c4eb040927922f73b133f13c720d81b04
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2013-12-23 22:16:40 +0900

    fix the bug that did not properly handle errors on getnameinfo().

--- lib/kcn_sockaddr.c
+++ lib/kcn_sockaddr.c
@@ -124,7 +124,7 @@ kcn_sockaddr_ntoa(char *name, size_t namelen, const struct sockaddr_storage *ss)
 
 	error = getnameinfo((struct sockaddr *)ss, sizeof(*ss), name, namelen,
 	    sbuf + 1, sizeof(sbuf - 1), NI_NUMERICHOST | NI_NUMERICSERV);
-	if (error == -1)
+	if (error != 0)
 		goto bad;
 	strlcat(name, sbuf, namelen);
 	return true;
